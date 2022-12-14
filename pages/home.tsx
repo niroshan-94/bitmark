7d44bf586d46a76c924fc99570ab1865ce866a28
niroshan-94 <120569104+niroshan-94@users.noreply.github.com>
2022-12-14 18:07:08 +0530

    Update and rename t.tsx to home.tsx

---
+++ pages/home.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+class Greetings extends React.Component {
+    state = {
+      iteration: 1,
+      title : "",
+      content :""
+    };
+    nextIter() {
+      let iter = this.state.iteration==100? 1 :this.state.iteration+1;
+      this.setState({ iteration: iter });
+    }
+
+    async callAPI(){
+        try {
+            const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${this.state.iteration}`);
+            const data = await res.json();
+            this.setState({ title: data.title });
+            this.setState({ content: data.body });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    render() {
+        return(
+            <div>
+                <span>{this.state.iteration}</span>
+                <h2>{this.state.title}</h2>
+                <p>{this.state.content}</p>
+                <button onClick={() => this.callAPI()}>Update</button>
+                <button onClick={() => this.nextIter()}>Set Next Iteration</button>
+            </div>
+        )
+    }
+  }
+
+
+function Home() {
+  return (
+    <div>
+    <Greetings />
+    </div>
+  )
+}
+
+export default Home
+
