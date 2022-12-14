626f02deb9acad49f73190c09169b9d7c3a4c448
niroshan-94 <120569104+niroshan-94@users.noreply.github.com>
2022-12-14 17:55:48 +0530

    Update signin.tsx

--- signin.tsx
+++ signin.tsx
@@ -1,5 +1,5 @@
 import React,{ useState, useEffect } from 'react'
-
+import api from './api/security'
 
 class Signin extends React.Component {
   state = {
@@ -16,42 +16,28 @@ class Signin extends React.Component {
 
   async callAPI(){
       try {
-          const res = await fetch('https://localhost/cisi.security/api/Authentication/SignIn', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                  "username": `${this.state.userName}`,
-                  "password": `${this.state.pass}`,
-                  "clientID": "CISI/2157",
-                  "scope": "api impersonate"
-                }),
-            });
-          const data = await res.json();
-          const res1 = await fetch('https://localhost/cisi.security/api/Authentication/Token', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                  "authToken": `${data.results.authToken}`,
-                  "clientID": "CISI/2157",
-                  "clientSecret": "70355fbe961b37e5e66c3081573f3a3b20543a521513a4ff4c41bd99669990f7"
-                }),
-            });
-          const data1 = await res1.json();
-          this.setState({ accessToken: data1.results.accessToken });
+        const t = await api(new URL('https://localhost/cisi.security/api/Authentication/SignIn'),{
+          "username": `${this.state.userName}`,
+          "password": `${this.state.pass}`,
+          "clientID": "CISI/2157",
+          "scope": "api impersonate"
+        });
+        const t1 = await api(new URL('https://localhost/cisi.security/api/Authentication/Token'),{
+          "authToken": `${t.results.authToken}`,
+          "clientID": "CISI/2157",
+          "clientSecret": "70355fbe961b37e5e66c3081573f3a3b20543a521513a4ff4c41bd99669990f7"
+        });
+        this.setState({ accessToken: t1.results.accessToken });
       } catch (err) {
           console.log(err);
       }
   };
 
-  setValue(content = "te") {
+  setValue(content = "") {
       this.setState({ userName: content });
   }
 
-  setValue1(content = "serf") {
+  setValue1(content = "") {
       this.setState({ pass: content });
   }
 
@@ -81,6 +67,3 @@ function signin() {
   );
 }
 export default signin
-
-
-
